dd9346ddd119656ca14717c2eae363c0a90157b3
proogeeks <dev@proogeeks.com>
2024-11-19 14:10:31 -0600

    [ADD] Portal expeditions

--- app/modules/expeditions/static/js/expeditions_products.js
+++ app/modules/expeditions/static/js/expeditions_products.js
@@ -1,45 +1,3 @@
-const selectProduct = (e) => {
-    const $elem = $(e.currentTarget);
-    const productId = $elem.data('id');
-    const name = $elem.data('name');
-    const qty = 1;
-
-    const $tbody = $('#order_product_list_content');
-    const $row = $(`
-        <tr data-product-id="${productId}">
-            <td>${name}</td>
-            <td class="text-right">
-                <input type="number" class="form-control form-control-sm text-right product_quantity_input"
-                       value="${qty}" min="1" style="width: 100px; float: right;">
-            </td>
-        </tr>
-    `);
-
-    $tbody.append($row);
-    updateProductIds();
-    updateSubmitButton();
-}
-
-const findProducts = async (e) => {
-    const searchTerm = $(e.currentTarget).val();
-    if(!searchTerm) return $('#select_product_list_container').html('');
-
-    const res = await jsonPost(`/expeditions/create/product/find`, { search: searchTerm }, { loading: false });
-    if(res?.status != 'success') return;
-
-    $('#select_product_list_container').html(res.data);
-    $('.o_portal_select_product_btn').click(selectProduct);
-}
-
-const updateProductIds = () => {
-    const productIds = [];
-    $('#order_product_list_content tr').each(function() {
-        const productId = $(this).data('product-id');
-        const quantity = $(this).find('.product_quantity_input').val();
-        productIds.push(`${productId}:${quantity}`);
-    });
-    $('.o_portal_product_ids').val(productIds.join(','));
-}
 
 const updateSubmitButton = () => {
     const hasProducts = $('#order_product_list_content tr').length > 0;
@@ -54,22 +12,83 @@ const updateSubmitButton = () => {
 }
 
 const showProductsModal = async () => {
-    const res = await jsonGet('/expeditions/products/list')
-    if(res?.status != 'success') return
+    const productIds = []
 
-    console.log(res.data)
+    const lines = $('.expedition-product-item-row')
+
+    for (let i = 0; i < lines.length; i++)
+        productIds.push($(lines[i]).data('id'));
+
+    const res = await jsonGet('/expeditions/products/list?pids=' + productIds.join(','))
+    if(res?.status != 'success') return
 
     $('#select_product_list_container').html(res.data)
     $('#modalproducts').modal('show')
 }
 
-// Event listeners
-$('.o_portal_add_products_btn').click(() => showProductsModal());
-$('#search_product_input').on('keyup', debounceAction(findProducts, 300));
-$('.o_portal_add_products_confirm_btn').click(() => $('#modalproducts').modal('hide'));
+const findProducts = async (e) => {
+    const searchTerm = $(e.currentTarget).val();
 
-$(document).on('change', '.product_quantity_input', function() {
-    updateProductIds();
-});
+    const productIds = []
+
+    const lines = $('.expedition-product-item-row')
+
+    for (let i = 0; i < lines.length; i++)
+        productIds.push($(lines[i]).data('id'));
+
+    const res = await jsonPost(`/expeditions/create/product/find`, { q: searchTerm, pids: productIds }, { loading: false });
+    if(res?.status != 'success') return;
+
+    $('#select_product_list_container').html(res.data);
+}
+
+const toggleAllProducts = (e) => {
+    const isChecked = $(e.currentTarget).prop('checked');
+    $('.stock-item-checkbox input[type="checkbox"]').prop('checked', isChecked);
+}
+
+const onAddProducts = async (e) => {
+    e.preventDefault();
+    const productIds = []
+
+    const lines = $('.stock-item-checkbox input[type="checkbox"]')
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = $(lines[i]);
+        if (line.is(':checked')) productIds.push(line.val());
+    }
+
+    const expeditionLines = $('.expedition-product-item-row')
+
+    for (let i = 0; i < expeditionLines.length; i++)
+        productIds.push($(expeditionLines[i]).data('id'));
+
+    if (productIds.length == 0) {
+        alert('No products selected');
+        return;
+    }
+
+    const res = await jsonPost('/expeditions/create/product/add', { product_ids: productIds }, { loading: false });
+
+    if(res?.status != 'success') return
+
+    $('#order_product_list_content').html(res.data)
+
+    $('#modalproducts').modal('hide')
+
+}
+
+const resetProductsModal = () => {
+    $('#search_product_input').val('')
+    $('#select_product_list_container').html('')
+}
+
+// Event listeners
+$('#modalproducts').on('hidden.bs.modal', resetProductsModal);
+$('#search_product_input').on('keyup', debounceAction(findProducts, 300));
+$(document).on('change', '#selectAll', toggleAllProducts);
+$('#js_products_form').on('submit', () => { return false });
+$('.o_portal_add_products_btn').click(() => showProductsModal());
+$('.o_portal_add_products_confirm_btn').click(onAddProducts);
 
 $('#client_account_id_select, #partner_shipping_id_select').change(updateSubmitButton);
