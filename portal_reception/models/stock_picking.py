3035bf7bd320ea59c076b5cf2f63177889b2807c
ProoGeeks <dev@proogeeks.com>
2025-06-18 13:26:27 -0600

    [ADD] Create portal receptions

---
+++ portal_reception/models/stock_picking.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright 2025-Today DaFe Solutions
+#
+##############################################################################
+
+from odoo import models, api
+
+
+class StockPicking(models.Model):
+    _name = 'stock.picking'
+    _inherit = ['stock.picking', 'portal.mixin']
+
+    def get_details_url(self):
+        portal_url = self.get_portal_url()
+        return portal_url.replace('#', f'/account/reception/details/{self.id}')
+
+    @api.returns('mail.message', lambda value: value.id)
+    def message_post(self, **kwargs):
+        res = super().message_post(**kwargs)
+        user = self.env['res.users'].sudo().search([('partner_id', '=', self.partner_id.id)],limit=1)
+        partner_ids = []
+        partner_ids.extend(self.partner_id.ids)
+        partner_ids.extend(self.user_id.partner_id.ids)
+        if user and user.partner_id.id in partner_ids:
+            user._bus_send( "portal_reception.reception_details_reload_request", { 'action': 'reload' } )
+        return res
