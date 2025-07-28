cd40182c775c8a8f886ae279fc44ad3bab195c15
ProoGeeks <dev@proogeeks.com>
2025-07-28 13:20:55 -0600

    [ADD] Portal expeditions catalog selection

--- portal_expedition/__manifest__.py
+++ portal_expedition/__manifest__.py
@@ -28,7 +28,7 @@
         'portal/portal_expedition_file_upload_modal.xml',
         'portal/expedition_templates.xml',
         'portal/expedition_details_page.xml',
-    ],   
+    ],
     # Es importante tener en cuenta el orden en el que deben declararse las carpetas y archivos
     'assets': {
     #     'web.assets_backend': [
