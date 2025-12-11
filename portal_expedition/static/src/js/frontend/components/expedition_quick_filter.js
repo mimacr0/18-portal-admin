7ffca4c4d2f03776cfa2ccd7bfe49f158e2359c2
AngelZH3 <angel.zh.3@gmail.com>
2025-12-11 11:57:49 +0100

    [ADD] General color

--- portal_expedition/static/src/js/frontend/components/expedition_quick_filter.js
+++ portal_expedition/static/src/js/frontend/components/expedition_quick_filter.js
@@ -51,13 +51,13 @@ export const initExpeditionQuickSortFilters = () => {
         // Actualizar el input con el valor del filtro
         activeFilterInput.value = filterValue;
         // Agregar clases para estilo activo
-        activeButton.classList.add('active', 'border-[#8A8A00]', 'text-[#696900]');
+        activeButton.classList.add('active', 'border-primary-theme', 'text-primary-theme');
         activeButton.classList.remove('border-transparent', 'text-gray-500');
 
         // Quitar clases de estilo activo a los otros botones
         inactiveButtons.forEach(button => {
             if (!button) return;
-            button.classList.remove('active', 'border-[#8A8A00]', 'text-[#696900]');
+            button.classList.remove('active', 'border-primary-theme', 'text-primary-theme');
             button.classList.add('border-transparent', 'text-gray-500');
         });
 
